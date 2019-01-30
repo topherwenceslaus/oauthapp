bf33bbecc819a9066cf65d78e15c1fc3956d6d4f
Christopher Wenceslaus <topher.wenceslaus@gmail.com>
2019-01-31 02:49:16 +0530

    initail

---
+++ src/components/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Layout from '../containers/Layout';
+
+const About = () => {
+  return (
+    <Layout>
+      <h2>About</h2>
+      <p>
+        Welcome to the jungle!!!! 
+        :)
+    
+      </p>
+    </Layout>
+  );
+};
+
+export default About;
